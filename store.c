e69e1479c634e8e4ff9c8c749dbae3b036324bac
r13 <rudrobhowmik13@gmail.com>
2026-08-06 07:29:47 +0600

    math added: completed

--- store.c
+++ store.c
@@ -1,20 +1,29 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#define print "print"
+#define print "lekh"
 #define var_in "varin"
 #define scan "scan"
-#define var_out "varout"
 #define if_else "jodi"
 #define loop "loop"
-FILE *f = fopen("rud.sec", "r");
+FILE *f;
 int isLoop = 0;
-
+char condi[50];
 char var[100][100];
 int in = -1;
 int val[100];
+void lineFree(char *line)
+{
+  int i = 0;
+  for (; i < 600; i++)
+  {
+    line[i] = '\0';
+  }
+  line[i + 1] = '\0';
+}
 int line_in(char *line, char last, FILE *source)
 {
+  lineFree(line);
   int i = 0;
   do
   {
@@ -27,20 +36,8 @@ int line_in(char *line, char last, FILE *source)
     line[i] = c;
     i++;
   } while (line[i - 1] != last);
-
-  // for (; ; i++)
-  // {
-  //   /* code */
-
-  // }
-  // printf("%d\n", i);
   return i;
 }
-// int call(FILE *f,char line[])
-// { int jk=*fgets(line,1024,f);//printf("%d\n", jk);
-//   return jk;
-
-// }
 int num_in(char *line, int *i)
 {
   int out = 0, m = 1;
@@ -58,7 +55,6 @@ void skip(char *line, int *i)
   for (; *(line + (*i)) == ' '; (*i)++)
   {
   }
-  // for(;line[i]==' ';i++){}
 }
 int var_finder(char *line, int *i)
 {
@@ -80,15 +76,60 @@ int var_finder(char *line, int *i)
   *i = b;
   return out;
 }
+int math(int index, char *line, int *i)
+{
+  char op;
+  int b = *i, n;
+  skip(line, &b);
+  op = line[b];
+  b++;
+  // printf("%c\n", op);
+  skip(line, &b);
+  // printf("%c\n", line[b]);
+  if (isdigit(line[b]))
+    n = num_in(line, &b);
+  if (isalpha(line[b]))
+    n = val[var_finder(line, &b)];
+  int ans, ind = 1;
+  switch (op)
+  {
+  case '+':
+    ans = 1;
+    break;
+  case '-':
+    ans = 1;
+    break;
+  case '*':
+    ans = 1;
+    break;
+  case '/':
+    ans = 1;
+    break;
+  case '%':
+    ans = val[index] % n;
+    break;
+
+  default:
+    ans = val[index];
+    ind = 0;
+    break;
+  }
+  if (ind == 1)
+    *i = b;
+  return ans;
+}
 int condition(char *line, int *i)
 {
   int l, r, ans = 0, b = *i;
   char op;
   skip(line, &b);
+  int start = b;
   if (isdigit(line[b]))
     l = num_in(line, &b);
   if (isalpha(line[b]))
-    l = val[var_finder(line, &b)];
+    // l = val[var_finder(line, &b)];   //simple, without math
+    l = math(var_finder(line, &b), line, &b);
+  // printf("%d\n", l);
   skip(line, &b);
   op = line[b];
   b++;
@@ -98,8 +139,15 @@ int condition(char *line, int *i)
   if (isdigit(line[b]))
     r = num_in(line, &b);
   if (isalpha(line[b]))
-    r = val[var_finder(line, &b)];
+    // r = val[var_finder(line, &b)];   //simple, without math
+    r = math(var_finder(line, &b), line, &b);
   // printf("%d\n", l);
+  int end = b;
+  for (int p = start, q = 0; p < end + 1; q++, p++)
+  {
+    condi[q] = line[p];
+  }
+
   switch (op)
   {
   case '<':
@@ -122,51 +170,57 @@ int condition(char *line, int *i)
   default:
     break;
   }
-  // printf("%d\n", b);
   *i = b;
   return ans;
 }
+void printLine(char line[])
+{
+  for (int o = 0; o < 100; o++)
+  {
+    printf("%c", line[o]);
+  }
+  printf("\n");
+  printf("\n");
+}
 void lexer(char line[], int i);
-void multiLine(char line[],int lineCount){
-      line_in(line, ',', f);
-      // printf("%c",line[d]);
-      // i=0;
-      char multiLine[30][1024];
-      int b = 0, d = 0, f = 0;
-      while (line[d] != ',')
+void multiLine(char line[], char last, int lineCount)
+{
+  char multiLine[30][1024];
+  int b = 0, d = 0, f = 0;
+  while (line[d] != last)
+  {
+    int indecator = 0;
+    while (line[d] != '\n')
+    {
+      if (line[d] == last)
       {
-        int indecator = 0;
-        while (line[d] != '\n')
-        {
-          if (line[d] == ',')
-          {
-            indecator = 1;
-            break;
-          }
-          multiLine[b][f] = line[d];
-          f++;
-          d++;
-        }
-        multiLine[b][f] = '\0';
-        if (indecator == 1)
-          break;
-        f = 0;
-        b++;
-        d++;
+        indecator = 1;
+        break;
       }
-      while (lineCount--)
-      {
-        for (int m = 0; m < b + 1; m++)
-        {
-          for (int k = 0; multiLine[m][k] != '\0'; k++)
-          {
-            // printf("%c", multiLine[m][k]);
-            line[k] = multiLine[m][k];
-          }
-          lexer(line, 0);
-        }
-      }
+      multiLine[b][f] = line[d];
+      f++;
+      d++;
     }
+    multiLine[b][f] = '\0';
+    if (indecator == 1)
+      break;
+    f = 0;
+    b++;
+    d++;
+  }
+  while (lineCount--)
+  {
+    for (int m = 0; m < b + 1; m++)
+    {
+      for (int k = 0; multiLine[m][k] != '\0'; k++)
+      {
+        line[k] = multiLine[m][k];
+      }
+      lexer(line, 0);
+      lineFree(line);
+    }
+  }
+}
 void lexer(char line[], int i)
 {
   skip(line, &i);
@@ -177,19 +231,22 @@ void lexer(char line[], int i)
     key[j] = line[i];
   }
   key[j] = '\0';
-  // printf("%s",key);
   if (strcmp(key, print) == 0)
   {
-    while (line[i] != '"')
+    skip(line, &i);
+    if (line[i] == '"')
     {
       i++;
+      for (; line[i] != '"'; i++)
+      {
+        printf("%c", line[i]);
+      }
+      printf("\n");
     }
-    i++;
-    for (; line[i] != '"'; i++)
+    else
     {
-      printf("%c", line[i]);
+      printf("%d\n", val[var_finder(line, &i)]);
     }
-    printf("\n");
   }
   else if (strcmp(key, var_in) == 0)
   {
@@ -207,8 +264,8 @@ void lexer(char line[], int i)
     // {
     //   val[in]+=(line[i]-'0')*m;m*=10;
     // }
+    skip(line, &i);
     val[in] = num_in(line, &i);
-    // printf("%d\n", val[in]);
   }
   else if (strcmp(key, scan) == 0)
   {
@@ -227,40 +284,24 @@ void lexer(char line[], int i)
     //   val[in]+=(line[i]-'0')*m;m*=10;
     // }
   }
-  else if (strcmp(key, var_out) == 0)
-  {
-    skip(line, &i);
-    printf("%d\n", val[var_finder(line, &i)]);
-  }
-  // edit
+  // else if (strcmp(key, var_out) == 0)
+  // {
+  //   skip(line, &i);
+  //   printf("%d\n", val[var_finder(line, &i)]);
+  // }
   else if (strcmp(key, if_else) == 0)
   {
-    // int c=condition(line,&i);
-    // c=1;
-    // printf("%d\n", c);
     if (condition(line, &i))
     {
-      for (; line[i] != ','; i++)
-      {
-      }
-      i++;
-      lexer(line, i);
-      if (line[i] == '\n')
-      {
-        line_in(line, '\n', f);
-      }
+      line_in(line, ',', f);
+      multiLine(line, ',', 1);
+      line_in(line, '.', f);
     }
     else
     {
-      // printf("%c\n", line[i]);
-      while (i++)
-      {
-        if (line[i] == ',' && line[i + 1] == ',')
-          break;
-      }
-      // for(;line[i]!=',' && line[i+1]!=',';i++){}
-      i += 3;
-      lexer(line, i);
+      line_in(line, ',', f);
+      line_in(line, '.', f);
+      multiLine(line, '.', 1);
     }
   }
   else if (strcmp(key, loop) == 0)
@@ -271,69 +312,46 @@ void lexer(char line[], int i)
       int c = num_in(line, &i);
       skip(line, &i);
       isLoop = 1;
-      // i++;
-      // while (line[i] != ",")
-multiLine(line, c);
-      // printf("%c",line[1]);
-      // while ()
+      line_in(line, '.', f);
+      multiLine(line, '.', c);
+      // for (int o = 0; o < 66; o++)
       // {
+      //   /* code */
+      //   printf("%c",line[o]);
       // }
-
-      // int a = 0, b = 0;
-      // while (c--)
-      // {
-      //   // line_in(multiLine, '', f);
-      //   while (line[i] != ",")
-      //   {
-
-      //   }
-      //   lexer(multiLine, 0);
-      // }
-      // printf("%c",line[i]);
+      // printf("\n");
       isLoop = 0;
     }
     else
     {
-      // int c=condition(line,&i);
-      // printf("%d\n", c);
-
       skip(line, &i);
-      isLoop = 1;
-    again:
-      // printf("%d\n", i);
-      lexer(line, i);
-      val[0]++;
-      if (condition(line, &i) == 1)
+      condition(line, &i);
+      while (condition(condi, &i))
       {
-        goto again;
+        line_in(line, '.', f);
+        multiLine(line, '.', 1);
       }
-
-      isLoop = 0;
+      //   isLoop = 1;
+      // isLoop = 0;
     }
   }
-  // printf("%c",line[i+3]);
-  if (isLoop == 0)
-  {
-    i += 100;
-    while (--i)
-    {
-      line[i] = ' ';
-    }
-  }
+  // if (isLoop == 0)
+  // {
+  //   i += 100;
+  //   while (--i)
+  //   {
+  //     line[i] = ' ';
+  //   }
+  // }
 }
 
 int main()
 {
-  // FILE *f = fopen("rud.sec", "r");
+  f = fopen("rud.sec", "r");
   char line[10240];
-
   // while(fgets(line,1024,f)){
   //   lexer(line,0);
   // }
-  // fgets(line,1024,f);
-
-  // printf("%c", line[2]);
-  // char m=*f;
 
   while (line_in(line, '\n', f))
   {
